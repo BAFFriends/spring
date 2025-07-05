6db482f2e381504031994d5d190a92cbeafa31c9
hann <wjdgks1233@gmail.com>
2025-07-05 16:40:34 +0900

    feat : create JpaAuditingConfig

---
+++ src/main/java/com/inner_medicine/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.inner_medicine.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
