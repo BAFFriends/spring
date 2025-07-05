c63d803e4b2b7d35067f5af38d3be6d14890d82d
hann <wjdgks1233@gmail.com>
2025-07-05 22:32:02 +0900

    feat : general exception #7

---
+++ src/main/java/com/inner_medicine/presentation/payload/exception/security/JwtAuthenticationException.java
@@ -0,0 +1,10 @@
+package com.inner_medicine.presentation.payload.exception.security;
+
+import com.inner_medicine.presentation.payload.code.ErrorStatus;
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(ErrorStatus errorStatus) {
+        super(errorStatus.name());
+    }
+}
