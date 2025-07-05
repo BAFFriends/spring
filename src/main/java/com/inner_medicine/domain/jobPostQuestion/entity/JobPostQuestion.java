74fbe8cf50ac374fd934c18c58dc83e9e9c2efac
김나경 <1030n@naver.com>
2025-07-05 20:22:02 +0900

    feat: JobPostQuestion Domain 추가

---
+++ src/main/java/com/inner_medicine/domain/jobPostQuestion/entity/JobPostQuestion.java
@@ -0,0 +1,42 @@
+package com.inner_medicine.domain.jobPostQuestion.entity;
+
+import com.inner_medicine.domain.auditing.entity.BaseTimeEntity;
+import com.inner_medicine.domain.company.entity.Company;
+import com.inner_medicine.domain.jobPost.entity.JobPost;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "jobPostQuestion")
+public class JobPostQuestion extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "jobPost_id", nullable = false)
+  private JobPost jobPost;
+
+  @Column(name = "question", nullable = false)
+  private String question;
+
+  @Column(name = "is_required", nullable = false)
+  private Boolean isRequired;
+}
