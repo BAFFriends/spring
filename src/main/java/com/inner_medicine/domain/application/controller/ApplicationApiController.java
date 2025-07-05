04338093f81d9add86e3a83edac1328079a810ba
seungjae708 <choesj365@gmail.com>
2025-07-06 01:11:07 +0900

    Feat: 지원서 생성: 이력서 + 답변 api 수정

--- src/main/java/com/inner_medicine/domain/application/controller/ApplicationApiController.java
+++ src/main/java/com/inner_medicine/domain/application/controller/ApplicationApiController.java
@@ -2,10 +2,13 @@ package com.inner_medicine.domain.application.controller;
 
 
 import com.inner_medicine.domain.application.dto.CreateApplicationRequest;
+import com.inner_medicine.domain.application.dto.JobPostAnswerRequest;
 import com.inner_medicine.domain.application.service.ApplicationCommandService;
 import com.inner_medicine.presentation.payload.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/applications")
@@ -14,13 +17,17 @@ public class ApplicationApiController {
 
     private final ApplicationCommandService applicationService;
 
-    @PostMapping
-    public ApiResponseDto<Void> createApplication(@RequestParam Long jobPostId,
-                                                  @RequestParam Long applicantId
-    ) {
+    @PostMapping("/{jobPostId}/{applicantId}")
+    public ApiResponseDto<Void> createApplication(@PathVariable Long jobPostId,
+                                                  @PathVariable Long applicantId,
+                                                  @RequestBody List<JobPostAnswerRequest> answerRequestList
+                                                  ) {
         CreateApplicationRequest request = new CreateApplicationRequest();
         request.setJobPostId(jobPostId);
         request.setApplicantId(applicantId);
+        request.setAnswers(answerRequestList);
+
+        applicationService.createApplication(request);
 
         return ApiResponseDto.onSuccess(null);
     }
