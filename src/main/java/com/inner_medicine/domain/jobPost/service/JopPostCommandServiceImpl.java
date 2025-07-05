19148a6ef314da2d77432c4118a55491e826938d
hann <wjdgks1233@gmail.com>
2025-07-06 00:18:05 +0900

    feat : write jobPost #12

---
+++ src/main/java/com/inner_medicine/domain/jobPost/service/JopPostCommandServiceImpl.java
@@ -0,0 +1,32 @@
+package com.inner_medicine.domain.jobPost.service;
+
+import com.inner_medicine.domain.company.entity.Company;
+import com.inner_medicine.domain.company.repository.CompanyRepository;
+import com.inner_medicine.domain.jobPost.dto.RequestJobPostDto;
+import com.inner_medicine.domain.jobPost.entity.JobPost;
+import com.inner_medicine.domain.jobPost.repository.JobPostRepository;
+import com.inner_medicine.presentation.payload.code.ErrorStatus;
+import com.inner_medicine.presentation.payload.exception.GeneralException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class JopPostCommandServiceImpl implements JobPostCommandService{
+
+    private final JobPostRepository jobPostRepository;
+    private final CompanyRepository companyRepository;
+
+
+    @Override
+    public Long writeJobPost(Long companyId, RequestJobPostDto requestJobPostDto) {
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.COMPANY_NOT_FOUND));
+        JobPost jobPost = requestJobPostDto.from();
+        jobPost.linkCompany(company);
+
+        return jobPostRepository.save(jobPost).getId();
+    }
+}
--- src/main/java/com/inner_medicine/presentation/payload/code/ErrorStatus.java
+++ src/main/java/com/inner_medicine/presentation/payload/code/ErrorStatus.java
@@ -27,7 +27,10 @@ public enum ErrorStatus implements BaseCode{
     AUTH_IS_NULL(BAD_REQUEST,4055, "토큰 값이 존재하지 않습니다.(null)"),
     AUTH_OAUTH2_EMAIL_NOT_FOUND_FROM_PROVIDER(NOT_FOUND, 4056, "해당 이메일은 카카오 계정 이메일로 존재하지 않습니다."),
     AUTH_MUST_AUTHORIZED_URI(BAD_REQUEST, 4057, "인증이 필수인 uri입니다."),
-    AUTH_ROLE_CANNOT_EXECUTE_URI(BAD_REQUEST,4058, "해당 인가로는 실행할 수 없는 동작입니다.");
+    AUTH_ROLE_CANNOT_EXECUTE_URI(BAD_REQUEST,4058, "해당 인가로는 실행할 수 없는 동작입니다."),
+
+    //company error (4100 ~ 4149)
+    COMPANY_NOT_FOUND(BAD_REQUEST, 4100, "회사를 찾을 수 없습니다");
 
 
 
