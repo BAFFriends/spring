553aa6484ea6667a6d12b5326288b5124eda3a55
hann <wjdgks1233@gmail.com>
2025-07-05 15:32:35 +0900

    init commit

---
+++ src/main/java/com/inner_medicine/InnerMedicineApplication.java
@@ -0,0 +1,13 @@
+package com.inner_medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerMedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerMedicineApplication.class, args);
+	}
+
+}
